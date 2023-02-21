579c38b127db348a83e7b72f228016587332923b
Sean Showalter <sean@showalter.design>
2023-02-21 14:31:53 +0000

    Tests passed

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,7 @@
 public class Application {
     /**
      * This class contains a main method that allows you to manually test the HardcodedLogin challenge functionality.
-     * No changes you make here affect the test case results. You must still run & pass the test cases.
+     * No changes you make here affect the test case results.  You must still run & pass the test cases.
      * All implementation code should be written in the Lab class.
      *
      * The Lab.login method should return true if a login should be permitted given two values.
